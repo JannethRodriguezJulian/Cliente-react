398318a89851be1a96c7adcef8622351b4fa8ba7
= <janeisabel05@gmail.com>
2021-03-05 07:13:16 -0400

    trii commit

--- src/server/EndPoints.js
+++ src/server/EndPoints.js
@@ -1,2 +1,2 @@
-export const CREATE_TASK = 'http://192.168.0.10:4000//api/users';
-export const SHOW_TASK = 'http://192.168.0.10:4000/api/users';
+export const CREATE_TASK = 'http://192.168.0.10:8000/todo';
+export const SHOW_TASK = 'http://192.168.0.10:8000/todo';
